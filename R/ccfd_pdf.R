e0824effb3e8a5d8a73ece4dc083ea209adad03a
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2016-04-01 14:11:55 +0200

    added TEXINPUTS to .R

--- R/ccfd_pdf.R
+++ R/ccfd_pdf.R
@@ -20,7 +20,7 @@
 ccfd_pdf <- function(...,
                              keep_tex = TRUE,
                              md_extensions = c("-autolink_bare_uris")) {
-  Sys.setenv(TEXINPUTS=system.file("rmarkdown","templates","ccfd_pdf","resources", package="ccfdinstr"))
+  Sys.setenv(TEXINPUTS=paste(system.file("rmarkdown","templates","ccfd_pdf","resources", package="ccfdinstr"),"",sep=":"))
   inherit_pdf_document(...,
                        template = find_resource("ccfd_pdf", "template.tex"),
                        keep_tex = keep_tex,
